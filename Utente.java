cdbb29208807f67866637d4cad9bcd1df092a009
MariaPacheco2001 <patcheco.f1@gmail.com>
2023-10-02 15:53:37 +0100

    Version 1 - Construtors

---
+++ Utente.java
@@ -0,0 +1,18 @@
+public class Utente {
+
+
+    private String nome;
+    private int idade;
+    private String genero;
+    private double altura;
+    private double peso;
+
+    public Utente(String nome, int idade, String genero, double altura, double peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.genero = genero;
+        this.altura = altura;
+        this.peso = peso;
+
+    }
+}
